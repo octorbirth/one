e297ae7b7741e7e818970cd18de4465c57bd5034
bit <bit@bitcamp>
2017-11-01 10:29:21 +0900

    modify 올림

--- spring/src/main/java/org/one/web/BoardController.java
+++ spring/src/main/java/org/one/web/BoardController.java
@@ -32,9 +32,11 @@ public class BoardController {
 	@GetMapping("/list")
 	public void list() { 
 		
-		
-		
-		/// list 콘트롤러 추가
+        return;
+    }
+	
+	@GetMapping("/modify")
+	public void modify() { 
 		
         return;
     }
